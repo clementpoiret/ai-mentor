212e9c34bdc7db92177d1e6c01c0a5a02dce70a9
Clément POIRET <poiret.clement@outlook.fr>
2023-06-18 23:10:16 +0200

    fix: annoying markdown format

--- src/ai/commands.ts
+++ src/ai/commands.ts
@@ -40,12 +40,12 @@ export const commands: Record<string, Command> = {
 			en: {
 				role: "system",
 				content:
-					"I took notes in a bullet-point format. Please organize all the notes, and provide (where possible), 1 or 2 paragraphs for each. Provide definitions or examples if complex ideas are present. All notes have to be in the redacted text. Do not provide any explanations, only the redacted text. Use Markdown to give subtitles. Emphasize important points in bold.",
+					"I took notes in a bullet-point format. Organize all the notes and provide 1 or 2 paragraphs for each. Provide definitions or examples if complex ideas are present. All notes have to be in the redacted text. Do not provide any explanations, only the redacted text. Use Markdown to give subtitles. Emphasize important points in bold.",
 			},
 			fr: {
 				role: "system",
 				content:
-					"J'ai pris des notes sous forme de liste à puces. Organise mes notes et rédige (si possible) 1 ou 2 paragraphes pour chaque point. Donne des définitions ou des exemples si des idées complexes sont présentes. Tous les points doivent être mentionnés dans le texte rédigé. Ne donne pas d'explications, juste le texte rédigé. Utilise du Markdown pour les sous-titres. Indique les points importants en gras.",
+					"J'ai pris des notes sous forme de liste à puces. Organise mes notes et rédige 1 ou 2 paragraphes pour chaque point. Donne des définitions ou des exemples si des idées complexes sont présentes. Tous les points doivent être mentionnés dans le texte rédigé. Ne donne pas d'explications, juste le texte rédigé. Utilise du Markdown pour les sous-titres. Indique les points importants en gras.",
 			},
 		},
 		pattern: [{ fr: 'Mes notes sont : "*"', en: 'My notes are: "*"' }],
@@ -65,19 +65,19 @@ export const commands: Record<string, Command> = {
 			en: {
 				role: "system",
 				content:
-					"I will provide you with a text and you will enhance it. You will correct any grammar, spelling, and syntax errors. Using upper-level English words and sentences, you will make my sentences more beautiful and elegant. Keep the exact same meaning. Any change you make must respect the original style of the text. The text is in Markdown, highlight ALL your changes in bold. When I need to rework the semantic content of a sentence in the original text, put it in italics. Only reply with the enhanced text. Provide explanations for your changes if and only if I ask you to do so.",
+					"I will provide you with a text and you will enhance it. You will correct any grammar, spelling, and syntax errors. Using upper-level English words and sentences, you will make my sentences more beautiful and elegant. Keep the exact same meaning. Any change you make must respect the original style of the text. Only reply with the enhanced text. Provide explanations for your changes if and only if I ask you to do so.",
 			},
 			fr: {
 				role: "system",
 				content:
-					"Je vais te donner un texte et tu vas l'améliorer. Tu vas corriger les fautes d'orthographe, grammaire et syntaxe. En utilisant des mots et phrases avec un meilleur niveau de français, tu vas rendre mes phrases plus belles et élégantes. Préserve le sens exact du texte. Chaque changement que tu fais doit respecter le style original du texte. Le texte est en Markdown, indique tous tes changements en gras. Quand je dois retravailler le contenu sémantique d'une phrase du texte original, mets-la en italique. Ne répond qu'avec le texte amélioré. N'explique tes changements que si et seulement si je te le demande.",
+					"Je vais te donner un texte et tu vas l'améliorer. Tu vas corriger les fautes d'orthographe, grammaire et syntaxe. En utilisant des mots et phrases avec un meilleur niveau de français, tu vas rendre mes phrases plus belles et élégantes. Préserve le sens exact du texte. Chaque changement que tu fais doit respecter le style original du texte. Ne répond qu'avec le texte amélioré. N'explique tes changements que si et seulement si je te le demande.",
 			},
 		},
 		pattern: [
 			{ fr: 'Améliore : "*"', en: 'Enhance: "*"' },
 			{
-				fr: "Explique-moi les changements que tu as fait",
-				en: "Explain to me the changes you have made",
+				fr: "Explique-moi les changements que tu as fait.",
+				en: "Explain to me the changes you have made.",
 			},
 		],
 		settings: {
